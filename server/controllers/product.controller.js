a176277304e9035e0dab2b951b6fd5db0edd31a2
ryan bautista <96922150+xryanb@users.noreply.github.com>
2022-05-16 12:40:04 -0700

    Add files via upload

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,22 @@
+const {Product}=require('../models/product.model')
+
+
+
+
+module.exports.index =(request,response) => {
+    response.json({
+        message:'Hello World lunch time?'
+    })
+}
+
+
+module.exports.createProduct=(request,response)=>{
+    const{title,price,description}= request.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product=>response.json(product))
+        .catch(err=>response.json(err))
+}
